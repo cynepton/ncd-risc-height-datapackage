32fa1f4692244fc37d119def08e56f007950d592
Isaac Aderonmu <aderonmuisaac@gmail.com>
2021-03-31 23:52:06 +0100

    feat: script to process downloaded file into data file

---
+++ scripts/process.py
@@ -0,0 +1,22 @@
+"""Clean original archive CSV files and save them to the data folder
+"""
+import csv
+
+sources = ['global', 'country-specific', 'region-specific']
+
+for key in sources:
+    data_list = []
+    with open('archive/{}.csv'.format(key), newline='') as archive_file:
+
+        for row in csv.DictReader(archive_file):
+            data_list.append(row)
+
+    # Methods to process the data would be called on the data_list object here
+
+    fieldnames = [item for item in data_list[0]]
+
+    with open('data/{}.csv'.format(key), 'w', newline='') as data_file:
+        writer = csv.DictWriter(data_file, fieldnames)
+
+        writer.writeheader()
+        writer.writerows(data_list)
